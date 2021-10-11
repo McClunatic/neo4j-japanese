205fde8786cd0d11e18db96475c391905cec5903
Brian McClune <bpmcclune@gmail.com>
2021-10-10 20:03:30 -0400

    Debug Cypher, add readings to graph

--- jmdict_parser.py
+++ jmdict_parser.py
@@ -4,7 +4,7 @@ import argparse
 import sys
 import textwrap
 
-from typing import List
+from typing import List, Union
 
 from lxml import etree
 from neo4j import GraphDatabase, Transaction
@@ -43,7 +43,7 @@ class NeoApp:
         self.close()
 
     def add_entry(self, entry: etree.Element) -> int:
-        """Adds an entry to the database.
+        """Adds an `entry` to the database.
 
         Args:
             entry: The entry element.
@@ -57,7 +57,7 @@ class NeoApp:
                 self._merge_and_return_entry,
                 ent_seq,
             )
-            print(f'Added entry with ent_seq {ent_seq}, node ID: {node_id}')
+            print(f'Added entry with ent_seq {ent_seq}, ID: {node_id}')
             return node_id
 
     def add_kanji_for_entry(
@@ -65,7 +65,7 @@ class NeoApp:
         kanji: etree.Element,
         entry: etree.Element,
     ) -> int:
-        """Adds an entry to the database.
+        """Adds `kanji` for `entry` to the database.
 
         Args:
             kanji: The k_ele kanji element.
@@ -90,7 +90,53 @@ class NeoApp:
                 ke_infs,
                 ke_pris,
             )
-            print(f'Added kanji {keb} to entry {ent_seq}, node ID: {node_id}')
+            print(f'Added kanji {keb} to entry {ent_seq}, ID: {node_id}')
+            return node_id
+
+    def add_reading_for_entry(
+        self,
+        reading: etree.Element,
+        entry: etree.Element,
+    ) -> int:
+        """Adds a `reading` for `entry` to the database.
+
+        Args:
+            reading: The r_ele reading element.
+            entry: The entry element.
+        """
+
+        # Get the word or phrase (reb)
+        reb = reading.find('reb').text
+
+        # Gather the information and priority codes
+        re_infs = [elem.text for elem in reading.findall('re_inf')]
+        re_pris = [elem.text for elem in reading.findall('re_pri')]
+
+        # Get whether a true reading for the entry
+        re_nokanji = reading.find('re_nokanji') is not None
+
+        # Get kebs that reading applies to (all if None)
+        re_restr = reading.find('re_restr')
+        if re_restr is not None:
+            re_restr = re_restr.text
+
+        # Get the ent_seq of the containing entry
+        ent_seq = entry.find('ent_seq').text
+
+        with self.driver.session() as session:
+            node_id, kanji = session.write_transaction(
+                self._merge_and_return_reading,
+                ent_seq,
+                reb,
+                re_nokanji,
+                re_restr,
+                re_infs,
+                re_pris,
+            )
+            print(
+                f'Added reading {reb} to entry {ent_seq}, '
+                f'(ID, kanji): {node_id}, {kanji}',
+            )
             return node_id
 
     @staticmethod
@@ -116,8 +162,10 @@ class NeoApp:
         # Add a node for the entry
         cypher = textwrap.dedent("""\
             MERGE (e:Entry {ent_seq: $ent_seq})
-            MERGE (k:Kanji {keb: $keb, ke_inf: $ke_infs, ke_pri: $ke_pris})
-            MERGE (e)-[:HAS_KANJI]->(k)
+            MERGE (k:Kanji {keb: $keb})
+            SET k.ke_inf = $ke_infs
+            SET k.ke_pri = $ke_pris
+            MERGE (e)-[:CONTAINS]->(k)
             RETURN id(k) AS node_id
         """)
         result = tx.run(
@@ -129,6 +177,56 @@ class NeoApp:
         )
         record = result.single()
         return record['node_id']
+
+    @staticmethod
+    def _merge_and_return_reading(
+        tx: Transaction,
+        ent_seq: int,
+        reb: str,
+        re_nokanji: bool,
+        re_restr: Union[str, None],
+        re_infs: List[str],
+        re_pris: List[str],
+    ) -> int:
+        """Merges and returns reading `reb` related to entry `ent_seq`."""
+
+        # Add a node for the entry
+        cypher = textwrap.dedent("""\
+            MERGE (e:Entry {ent_seq: $ent_seq})
+            MERGE (r:Reading {reb: $reb})
+            SET r.re_inf = $re_infs
+            SET r.re_pri = $re_pris
+            SET r.re_nokanji = $re_nokanji
+            MERGE (e)-[:CONTAINS]->(r)
+            RETURN id(r) AS node_id
+        """)
+        result = tx.run(
+            cypher,
+            ent_seq=ent_seq,
+            reb=reb,
+            re_nokanji=re_nokanji,
+            re_infs=re_infs,
+            re_pris=re_pris,
+        )
+        record = result.single()
+
+        # Add kanji reading relationships
+        cypher = textwrap.dedent("""\
+            MATCH (e:Entry {ent_seq: $ent_seq})
+            OPTIONAL MATCH (e)-[:CONTAINS]->(k:Kanji)
+            WITH e, k
+            WHERE k IS NOT NULL AND k.keb = coalesce($re_restr, k.keb)
+            MERGE (k)-[r:HAS_READING]->(:Reading {reb: $reb})
+            RETURN id(k) AS node_id
+        """)
+        result = tx.run(
+            cypher,
+            ent_seq=ent_seq,
+            reb=reb,
+            re_restr=re_restr,
+        )
+        values = [record.values() for record in result]
+        return record['node_id'], values
 
 
 def get_parser(argv: List[str]) -> argparse.ArgumentParser:
@@ -178,8 +276,10 @@ def main(argv=sys.argv[1:]):
         for k_ele in entry.findall('k_ele'):
             neo_app.add_kanji_for_entry(k_ele, entry)
 
+        for r_ele in entry.findall('r_ele'):
+            neo_app.add_reading_for_entry(r_ele, entry)
+
         # TODO: Walk and handle the kanji, reading, and sense elements
-        # readings = entry.findall('r_ele')
         # senses = entry.findall('sense')
 
     # Close the neo_app
