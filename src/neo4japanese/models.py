787211360ea34e7549f9415c1be11d5b9da3b5c0
Brian McClune <bpmcclune@gmail.com>
2021-11-24 07:21:50 -0500

    Update models and jmdict parser for <sense>

--- src/neo4japanese/models.py
+++ src/neo4japanese/models.py
@@ -1,17 +1,20 @@
 """Defines node models for the Neo4j for Japanese database."""
 
-from typing import List
+from enum import Enum
+from typing import List, Optional
 
 from pydantic import BaseModel
 
 
 class Kanji(BaseModel):
+    """Model for Kanji node."""
     keb: str
     ke_infs: List[str]
     ke_pris: List[str]
 
 
 class Reading(BaseModel):
+    """Model for Reading node."""
     reb: str
     re_nokanji: bool
     re_restr: List[str]
@@ -19,7 +22,63 @@ class Reading(BaseModel):
     re_pris: List[str]
 
 
+class Language(BaseModel):
+    """Model for Language node."""
+    name: str
+
+
+class Lsource(BaseModel):
+    """Model for Sense->Language relationship."""
+    lang: Language
+    phrase: str
+    partial: bool
+    wasei: bool
+
+
+class Gloss(BaseModel):
+    """Model for Gloss node."""
+    defn: List[str]
+    expl: List[str]
+    fig: List[str]
+    lit: List[str]
+    tm: List[str]
+
+
+class SourceType(Enum):
+    """Model for recognized example source types."""
+    tat = 'tat'
+
+
+class Sentence(BaseModel):
+    """Model for Sentence node."""
+    exsrc_type: SourceType
+    ex_srce: int
+    eng: str
+    jpn: str
+
+
+class Example(BaseModel):
+    """Model for Sense->Sentence relationship."""
+    sentence: Sentence
+    ex_text: str
+
+
+class Sense(BaseModel):
+    """Model for Sense node."""
+    stagk: List[str]
+    stagr: List[str]
+    pos: List[str]
+    field: List[str]
+    misc: List[str]
+    s_inf: List[str]
+    lsource: List[Lsource]
+    dial: List[str]
+    gloss: Optional[Gloss]
+    example: List[Example]
+
+
 class Entry(BaseModel):
+    """Model for Entry node."""
     ent_seq: int
     k_ele: List[Kanji]
     r_ele: List[Reading]
--- src/neo4japanese/parsers/jmdict.py
+++ src/neo4japanese/parsers/jmdict.py
@@ -4,11 +4,24 @@ See `<http://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project>`_
 for more information.
 """
 
-from typing import List
+from typing import List, Optional
 
 from lxml import etree
 
-from ..models import Entry, Kanji, Reading
+from ..models import (
+    Entry,
+    Example,
+    Gloss,
+    Kanji,
+    Language,
+    Lsource,
+    Reading,
+    Sense,
+    Sentence,
+)
+
+# Standard XML namespace
+XML_NAMESPACE = 'http://www.w3.org/XML/1998/namespace'
 
 
 def get_kanji(entry: etree.Element) -> List[Kanji]:
@@ -18,7 +31,7 @@ def get_kanji(entry: etree.Element) -> List[Kanji]:
         entry: The ``<entry>`` element to parse.
 
     Returns:
-        A List of kanji objects.
+        A list of kanji objects.
     """
 
     kanji = []
@@ -39,7 +52,7 @@ def get_readings(entry: etree.Element) -> List[Reading]:
         entry: The ``<entry>`` element to parse.
 
     Returns:
-        A List of kanji objects.
+        A list of reading objects.
     """
 
     readings = []
@@ -53,6 +66,116 @@ def get_readings(entry: etree.Element) -> List[Reading]:
         ))
 
     return readings
+
+
+def get_lsources(sense: etree.Element) -> List[Lsource]:
+    """Returns list of :class:`~neo4japanese.models.Lsource` for `sense`.
+
+    Args:
+        sense: The ``<sense>`` element to parse.
+
+    Returns:
+        A list of lsource objects.
+    """
+
+    lsources = []
+    for lsource in sense.xpath('lsource'):
+        name = lsource.attrib.get(f'{{{XML_NAMESPACE}}}lang', 'eng'),
+        lsources.append(Lsource(
+            lang=Language(name=name),
+            phrase=lsource.text,
+            partial=lsource.attrib.get('ls_type', 'full') == 'partial',
+            wasei=lsource.attrib.get('ls_wasei', 'n') == 'y',
+        ))
+
+    return lsources
+
+
+def get_gloss(sense: etree.Element) -> Optional[Gloss]:
+    """Returns gloss of :class:`~neo4japanese.models.Gloss` for `sense`.
+
+    Args:
+        sense: The ``<sense>`` element to parse.
+
+    Returns:
+        A gloss object (or ``None`` if not present in `sense`).
+    """
+
+    fields = dict(
+        defn=[elem.text for elem in sense.xpath('gloss[not(@g_type)]')],
+        expl=[elem.text for elem in sense.xpath('gloss[@g_type="expl"]')],
+        fig=[elem.text for elem in sense.xpath('gloss[@g_type="fig"]')],
+        lit=[elem.text for elem in sense.xpath('gloss[@g_type="lit"]')],
+        tm=[elem.text for elem in sense.xpath('gloss[@g_type="tm"]')],
+    )
+    return Gloss(**fields) if any(fields.values()) else None
+
+
+def get_examples(sense: etree.Element) -> List[Example]:
+    """Returns list of :class:`~neo4japanese.models.Example` for `sense`.
+
+    Args:
+        sense: The ``<sense>`` element to parse.
+
+    Returns:
+        A list of example objects.
+    """
+
+    def lang(elem):
+        """Helper function to get xml:lang attribute of <ex_sent>."""
+        return elem.attrib.get(f'{{{XML_NAMESPACE}}}lang', 'eng')
+
+    examples = []
+    for example in sense.xpath('lsource'):
+        ex_srce = example.find('ex_srce')
+        ex_sents = {lang(elem): elem.text for elem in example.xpath('ex_sent')}
+        examples.append(Example(
+            sentence=Sentence(
+                exsrc_type=ex_srce.attrib.get('exsrc_type'),
+                ex_srce=ex_srce.text,
+                eng=ex_sents['eng'],
+                jpn=ex_sents['jpn'],
+            ),
+            ex_text=example.find('ex_text').text,
+        ))
+
+    return examples
+
+
+def get_senses(entry: etree.Element) -> List[Sense]:
+    """Returns list of :class:`~neo4japanese.models.Sense` for `entry`.
+
+    Args:
+        entry: The ``<entry>`` element to parse.
+
+    Returns:
+        A list of sense objects.
+    """
+
+    senses = []
+    for sense in entry.xpath('sense'):
+        stagk = (
+            [elem.text for elem in sense.xpath('stagk')] or
+            [elem.text for elem in entry.xpath('.//keb')]
+        )
+        stagr = (
+            [elem.text for elem in sense.xpath('stagr')] or
+            [elem.text for elem in entry.xpath('.//reb')]
+        )
+        senses.append(Sense(
+            stagk=stagk,
+            stagr=stagr,
+            pos=[elem.text for elem in sense.xpath('pos')],
+            field=[elem.text for elem in sense.xpath('field')],
+            misc=[elem.text for elem in sense.xpath('misc')],
+            s_inf=[elem.text for elem in sense.xpath('s_inf')],
+            lsource=get_lsources(sense),
+            dial=[elem.text for elem in sense.xpath('dial')],
+            gloss=get_gloss(sense),
+            example=get_examples(sense),
+        ))
+
+    return senses
 
 
 def get_entry(entry: etree.Element) -> Entry:
